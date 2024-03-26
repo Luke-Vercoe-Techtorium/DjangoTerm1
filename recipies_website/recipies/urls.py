be9501ce42a59df4001fefc73c450db603636b43
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2024-03-26 14:40:50 +1300

    36

--- recipies_website/recipies/urls.py
+++ recipies_website/recipies/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.RecipieListView.as_view(), name="recipies-home"),
+    path("recipe/<int:pk>", views.RecipieDetailView.as_view(), name="recipies-detail"),
+    path("recipe/create", views.RecipieCreateView.as_view(), name="recipies-create"),
+    path("recipe/<int:pk>/update", views.RecipieUpdateView.as_view(), name="recipies-update"),
+    path("recipe/<int:pk>/delete", views.RecipieDeleteView.as_view(), name="recipies-delete"),
 ]
--- recipies_website/recipies/views.py
+++ recipies_website/recipies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from . import models
 
 class RecipieListView(ListView):
@@ -8,7 +10,33 @@ class RecipieListView(ListView):
     template_name ="recipies/home.html"
     context_object_name = "recipies"
 
-def about(request: HttpRequest) -> HttpResponse:
-    return render(request, "recipies/about.html", {
-        "title": "About Page",
-    })
+class RecipieDetailView(DetailView):
+    model = models.Recipie
+
+class RecipieDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = models.Recipie
+    success_url = reverse_lazy("recipies-home")
+
+    def test_func(self):
+        recipie = self.get_object()
+        return self.request.user == recipie.author
+
+class RecipieCreateView(LoginRequiredMixin, CreateView):
+    model = models.Recipie
+    fields = ['title', 'description']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class RecipieUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = models.Recipie
+    fields = ['title', 'description']
+
+    def test_func(self):
+        recipie = self.get_object()
+        return self.request.user == recipie.author
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
